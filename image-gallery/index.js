8c19d01a51dd856a50a34c9b9b571284f13b8b96
Victoria Stratulat <victoria.stratulat@gmail.com>
2024-09-29 22:22:50 +0300

    feat: add search function

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,5 +1,9 @@
-const url = "https://api.unsplash.com/photos/?client_id=_VSHgXLdm9Jt7rhh099EzGb8koVHaMPLZiDQ9osMk2k";
 const galleryContainer = document.querySelector(".container");
+const SEARCHbtn = document.querySelector('.search-button');
+const INPUT = document.querySelector('.search-field');
+const url = "https://api.unsplash.com/photos/?client_id=_VSHgXLdm9Jt7rhh099EzGb8koVHaMPLZiDQ9osMk2k";
+
+INPUT.focus();
 
 async function getData() {
     const res = await fetch(url);
@@ -13,7 +17,18 @@ async function getData() {
 
   function showData(data) {
     const div = `<div class="container-img" style="background-image: url(${data.urls.regular});"></div>`;
-  galleryContainer.insertAdjacentHTML("beforeend", div1);
+  galleryContainer.insertAdjacentHTML("beforeend", div);
   }
 
-  
+  function searchImages (term) {
+    console.log('search clicked');
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${term}`
+    fetch(url)
+        .then(data => {
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        showData(data[i]);
+  }
+  })};
+
+SEARCHbtn.addEventListener('click', () => {console.log("clicked", searchImages(INPUT.value))});
