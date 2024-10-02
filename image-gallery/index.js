b70cd498737d7b3cda5d4f091dea8d9c9701e8f0
Victoria Stratulat <victoria.stratulat@gmail.com>
2024-10-02 19:49:10 +0300

    fix: fix search function

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,34 +1,38 @@
 const galleryContainer = document.querySelector(".container");
 const SEARCHbtn = document.querySelector('.search-button');
 const INPUT = document.querySelector('.search-field');
-const url = "https://api.unsplash.com/photos/?client_id=_VSHgXLdm9Jt7rhh099EzGb8koVHaMPLZiDQ9osMk2k";
+const url = "https://api.unsplash.com/photos/";
+const KEY = '?client_id=_VSHgXLdm9Jt7rhh099EzGb8koVHaMPLZiDQ9osMk2k'
 
 INPUT.focus();
 
 async function getData() {
-    const res = await fetch(url);
+  const url_1 = url+KEY;
+    const res = await fetch(url_1);
     const data = await res.json();
     console.log(data[0]);
-    for (let i = 0; i < data.length; i++) {
-        showData(data[i]);
-  }}
+    showData(data);
+  }
 
   getData();
 
   function showData(data) {
-    const div = `<div class="container-img" style="background-image: url(${data.urls.regular});"></div>`;
-  galleryContainer.insertAdjacentHTML("beforeend", div);
-  }
-
-  function searchImages (term) {
-    console.log('search clicked');
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${term}`
-    fetch(url)
-        .then(data => {
     for (let i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        showData(data[i]);
+    const div = `<div class="container-img" style="background-image: url(${data[i].urls.regular});"></div>`;
+  galleryContainer.insertAdjacentHTML("beforeend", div);
+    }
   }
-  })};
 
-SEARCHbtn.addEventListener('click', () => {console.log("clicked", searchImages(INPUT.value))});
+  async function searchImages (term) {
+    console.log('search clicked');
+    const url_1 = 'https://api.unsplash.com/search/photos'+KEY+`&page=1&query=${term}`;
+    const res = await fetch(url_1);
+    const data = await res.json();
+    console.log(data.results);
+    galleryContainer.innerHTML = '';
+    showData(data.results);
+  };
+
+SEARCHbtn.addEventListener('click', () => {
+  console.log("clicked", INPUT.value); 
+  searchImages(INPUT.value)});
